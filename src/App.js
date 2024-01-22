29ad40dc8c2b88d669a879c5277a0e5e81affc9d
Melik Ben Salah <melikbensalah2003@gmail.com>
2024-01-22 18:56:34 +0100

    First commit
    date : 22/01/2024

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Menu from './Components/Menu';
+import Footer from './Components/Footer';
+
+
+function App() {
+  return (
+
+      <div>
+        <Nav />
+        <Main />
+        <Menu />
+        <Footer />
+
+      </div>
+
+  );
+}
+
+export default App;
+
